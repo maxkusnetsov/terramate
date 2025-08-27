03657fd2fc2bbf2be33e3fe9a6ed47df326dcd40
maxkusnetsov <github@maxkusnetsov.me>
2025-08-27 11:23:42 +0400

    Add logs about outputs

--- engine/run.go
+++ engine/run.go
@@ -362,7 +362,7 @@ func (e *Engine) RunAll(
 						cmd.Stdout = &stdout
 						cmd.Stderr = &stderr
 						cmd.Dir = otherStack.HostDir(e.Config())
-            logger.Warn().Interface("cmd", cmd).Msg("cmd")
+            logger.Warn().Interface("dir", cmd.Dir).Msg("cmddir")
 						var inputVal cty.Value
 						err := cmd.Run()
 						if err != nil {
